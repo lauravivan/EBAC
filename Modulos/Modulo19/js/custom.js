8cd831f114230d7e2707a53596aa8a427804e6b6
Laura <lauravivangoncalves@gmail.com>
2022-08-01 16:31:20 -0300

    Continuação do módulo 18 com inserção de formulários e jquery + ajustes na responsividade

---
+++ Modulos/Modulo19/js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $('#about-us').click(function() {
+
+        $('#navbarResponsive').removeClass('show')
+
+    })
+
+    $('#contact').click(function() {
+
+        $('#navbarResponsive').removeClass('show')
+
+    })
+
+})
